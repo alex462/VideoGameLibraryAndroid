2c6093a70a6aa781844bb020b7bd3da853b5f9c0
alex462 <alex.reinhart462@gmail.com>
2018-04-20 03:54:36 -0400

    created fragment_add_game.xml, AddGameFragment, and VideoGameAdapter.

---
+++ app/src/main/java/com/example/alexandrareinhart/videogamelibrary/AddGameFragment.java
@@ -0,0 +1,91 @@
+package com.example.alexandrareinhart.videogamelibrary;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Date;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class AddGameFragment extends Fragment {
+
+    private ActivityCallback activityCallback;
+    private VideoGameDatabase videoGameDatabase;
+
+    @BindView(R.id.enter_title_editText)
+    protected EditText gameTitle;
+
+    @BindView(R.id.enter_genre_editText)
+    protected EditText gameGenre;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        
+        View view = inflater.inflate(R.layout.item_video_game, container, false);
+        ButterKnife.bind(this, view);
+        
+        return view;
+    }
+
+    public static AddGameFragment newInstance() {
+        
+        Bundle args = new Bundle();
+        
+        AddGameFragment fragment = new AddGameFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        videoGameDatabase = ((VideoGameApplication) getActivity().getApplicationContext()).getDatabase();
+    }
+
+    @OnClick(R.id.add_game_fab)
+    protected void addButtonClicked(){
+
+        if(gameTitle.getText().toString().isEmpty() || gameGenre.getText().toString().isEmpty()){
+            Toast.makeText(getActivity(), "ALL FIELDS ARE REQUIRED", Toast.LENGTH_LONG).show();
+        }
+        else{
+
+            VideoGame videoGame = new VideoGame(gameTitle.getText().toString(), gameGenre.getText().toString(), new Date());
+
+            addGameToDatabase(videoGame);
+        }
+    }
+
+    private void addGameToDatabase(final VideoGame videoGame) {
+
+        videoGameDatabase.videoGameDao().addVideoGame(videoGame);
+        activityCallback.addClicked();
+
+        Toast.makeText(getActivity(), "GAME ADDED", Toast.LENGTH_LONG).show();
+    }
+
+    private void addGameToDatabase(){
+
+
+    }
+
+    public void attachParent(ActivityCallback activityCallback){
+
+        this.activityCallback = activityCallback;
+    }
+
+    public interface ActivityCallback{
+
+        void addClicked();
+    }
+}
--- app/src/main/java/com/example/alexandrareinhart/videogamelibrary/MainActivity.java
+++ app/src/main/java/com/example/alexandrareinhart/videogamelibrary/MainActivity.java
@@ -2,12 +2,68 @@ package com.example.alexandrareinhart.videogamelibrary;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity implements VideoGameAdapter.AdapterCallback, AddGameFragment.ActivityCallback {
+
+    @BindView(R.id.game_recycler_view)
+    protected RecyclerView recyclerView;
+
+    private VideoGameDatabase videoGameDatabase;
+    private VideoGameAdapter videoGameAdapter;
+    private AddGameFragment addGameFragment;
+    private LinearLayoutManager linearLayoutManager;
+    //    private List<VideoGame> videoGameList;
+    /**
+     * ASK CRYSTAL: This List is called videoGameList, but there is another list in the program called videoGamesList that we have been utilizing up to this point. Are they 2 different lists or the same? NEED TO FIX.
+     */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        videoGameDatabase = ((VideoGameApplication) getApplicationContext()).getDatabase(); //explicitly tell Android to go into VGA to get app context, since that is where we (developer) established context, but not where Android would normally get app context from.
+        /**
+         * ASK CRYSTAL: Where does Android "normally" get app context from if not specified by developer?
+         */
+
+        setUpRecyclerView();
+    }
+
+    private void setUpRecyclerView() {
+
+        linearLayoutManager = new LinearLayoutManager(getApplicationContext());
+
+        videoGameAdapter = new VideoGameAdapter(videoGameDatabase.videoGameDao().getVideoGames(), this);
+
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setAdapter(videoGameAdapter);
+
+        videoGameAdapter.notifyDataSetChanged();
+    }
+
+    @OnClick(R.id.add_game_button)
+    protected void addGameButtonClicked(){
+
+        addGameFragment = AddGameFragment.newInstance();
+        addGameFragment.attachParent(this);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_holder, addGameFragment).commit();
+    }
+
+
+    @Override
+    public void addClicked() {
+
+        getSupportFragmentManager().beginTransaction().remove(addGameFragment).commit();
+        videoGameAdapter.updateList(videoGameDatabase.videoGameDao().getVideoGames());
     }
 }
