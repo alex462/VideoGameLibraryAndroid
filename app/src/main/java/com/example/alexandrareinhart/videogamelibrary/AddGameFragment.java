fbe62cbd86050ebadb980a5b3803c450fe94b21a
alex462 <alex.reinhart462@gmail.com>
2018-05-15 13:55:16 -0400

    Fixed dependencies in app gradle. Program runs.

--- app/src/main/java/com/example/alexandrareinhart/videogamelibrary/AddGameFragment.java
+++ app/src/main/java/com/example/alexandrareinhart/videogamelibrary/AddGameFragment.java
@@ -4,15 +4,14 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.constraint.ConstraintLayout;
+
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import java.util.Date;
-
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
